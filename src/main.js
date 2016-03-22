12e1c73a7eb5c0a87ca2032839fa97658b474c54
James <Jlassos@gmail.com>
2016-03-21 20:14:02 -0700

    ugh

--- src/main.js
+++ src/main.js
@@ -56,6 +56,7 @@ app.get('/package',(req, res) => {
         res.json(req.body)
     })
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
     console.log('Express app listening on port 3000')
 })
